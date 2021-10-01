81fd56e88c3b48486d9bf8156353ced9d2ea9246
susanisaksson <susan.isaksson@medieinstitutet.se>
2021-10-01 13:30:53 +0200

    finjustering styling cart-sidan

--- public/cart.js
+++ public/cart.js
@@ -2,6 +2,8 @@
 let stripe = Stripe('pk_test_51JZwfhIMTHb0TS9a5j5WbnuPHSwsdmvhjvM4FCmlQP9L6dKJhsSekUu99eCFTAy0pqMeHyGAQSejOfPCzlLyd3TL006JYP3wyJ');
 //let stripe = Stripe('pk_test_51JbhAtI15NR3oivl1Rxdgpnad3GN14mR2OTtJbM2e6VNPEa1cYL7PTMdHBlpU2aUGa4ncdbvUyBiUZ16303LmKq100BkngM59V');
 
+
+
 async function checkoutBtn() {
 
     let cart = JSON.parse(localStorage.getItem("cart"));
@@ -35,26 +37,37 @@ async function printFromLocal() {
     document.getElementById("cartCardDiv").innerText = ""
     let cart = JSON.parse(localStorage.getItem("cart"))
 
+    
     if (cart == null) {
-        //cart = {}
-        document.getElementById("emptyCart").innerText = "Din varukorg är tom, gå till..."
-        return       
+     
+        document.getElementById("emptyCart").innerText = "Din varukorg är tom!"
+        
+        return     
+          
     }
+    document.getElementById("emptyCart").innerText = "Du har valt följande klänning:"
+   
 
+  
+  
     let amount = 0;
     let counter = 0;
     for (const key in cart) {
+        
         if (Object.hasOwnProperty.call(cart, key)) {
             const cartRow = cart[key];
             counter += cartRow.quantity
             amount += cartRow.price_data.unit_amount / 100 * cartRow.quantity
         }
+       
     }
-    document.getElementById("totalPriceDiv").innerText = "Totalt pris: " + amount
+    document.getElementById("totalPriceDiv").innerText = "Totalt pris: " + amount + " kr"
 
-
+    
 
     Object.keys(cart).forEach(key => {
+
+        
 
         let cartCardDiv = document.getElementById("cartCardDiv")
 
